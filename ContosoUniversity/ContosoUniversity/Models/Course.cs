2bb26a025a82a35df26d47f13001b6c142201d97
yavaka <yavakaa@gmail.com>
2020-02-16 11:15:23 +0000

    Add Course model

---
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        //The DatabaseGenerated attribute allows the app to specify the primary key rather than having the database generate it.
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
