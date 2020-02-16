d31949cdb5fc9064d817b3cd39aeadaa60d9e325
yavaka <yavakaa@gmail.com>
2020-02-16 12:40:41 +0000

    List enrollments in Details view of Student

--- ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
@@ -33,6 +33,8 @@ namespace ContosoUniversity.Controllers
                 return NotFound();
             }
 
+            //The Include and ThenInclude methods cause the context to load the Student.
+            //Enrollments navigation property, and within each enrollment the Enrollment.Course navigation property.
             var student = await _context.Students
                 .Include(s => s.Enrollments)
                 .ThenInclude(e => e.Course)
